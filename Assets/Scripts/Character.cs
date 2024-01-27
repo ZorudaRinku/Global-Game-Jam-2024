c2401c9da26223b00cb6e37fd7f4eaf2c715704b
RussAMR <44710713+RussAMR@users.noreply.github.com>
2024-01-26 22:08:40 -0700

    Test Scene Created + Character Scripts
    Added Character Script with basic movement and health functionality.
    Created a small test scene to work on the character object outside of sample scene (TBDL)

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float health = 1;
+    Vector2 movement;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            health = health - -0.5f;
+            if (health == 0)
+            {
+                GameObject.Destroy(this.gameObject);
+            }
+        }
+    }
+}
