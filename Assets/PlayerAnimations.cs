96dbc6add0624bba15519ef96d9ae984518d33f8
ZorudaRinku <stronger6@gmail.com>
2024-01-28 00:38:47 -0700

    Player Interact NPC Tag Animation leave off on last frame

--- Assets/PlayerAnimations.cs
+++ Assets/PlayerAnimations.cs
@@ -15,36 +15,39 @@ public class PlayerAnimations : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Ugly
+        _animator.speed = Character.movement.magnitude <= 0.10 ? 0 : 1;
+        
         switch (Character.movement.y)
         {
-            case > 0:
+            case > 0 when Character.movement.x == 0:
                 _animator.SetBool("WalkUp", true);
-                break;
-            case < 0:
-                _animator.SetBool("WalkDown", true);
-                break;
-            default:
-                _animator.SetBool("WalkUp", false);
                 _animator.SetBool("WalkDown", false);
+                _animator.SetBool("WalkRight", false);
+                _animator.SetBool("WalkLeft", false);
                 break;
-        }
-        switch (Character.movement.x)
-        {
-            case > 0:
-                _animator.SetBool("WalkRight", true);
-                break;
-            case < 0:
-                _animator.SetBool("WalkLeft", true);
-                break;
-            default:
+            case < 0 when Character.movement.x == 0:
+                _animator.SetBool("WalkDown", true);
+                _animator.SetBool("WalkUp", false);
                 _animator.SetBool("WalkRight", false);
                 _animator.SetBool("WalkLeft", false);
                 break;
         }
 
-        //if (Character.isAttacking)
-        //{
-            
-        //}
+        switch (Character.movement.x)
+        {
+            case > 0 when Character.movement.y == 0:
+                _animator.SetBool("WalkRight", true);
+                _animator.SetBool("WalkLeft", false);
+                _animator.SetBool("WalkUp", false);
+                _animator.SetBool("WalkDown", false);
+                break;
+            case < 0 when Character.movement.y == 0:
+                _animator.SetBool("WalkLeft", true);
+                _animator.SetBool("WalkRight", false);
+                _animator.SetBool("WalkUp", false);
+                _animator.SetBool("WalkDown", false);
+                break;
+        }
     }
 }
